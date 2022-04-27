ce40f14fd3e1ea787570f351941007bbbb87dd5a
MinseokChoi <alex0316@gmail.com>
2022-04-27 16:20:40 +0900

    Feat: update models

--- products/models.py
+++ products/models.py
@@ -2,16 +2,16 @@ from django.db    import models
 from users.models import User
 
 class Product(models.Model):
-    size           = models.IntegerField(max_length=100)
-    description    = models.CharField(max_length=100)
-    price          = models.DecimalField(max_digits=6, decimal_places=2)
+    size           = models.IntegerField()
+    description    = models.CharField(max_length=1000)
+    price          = models.DecimalField(max_digits=8, decimal_places=2)
     category       = models.ForeignKey('Category', on_delete=models.CASCADE)
-    alcohol_degree = models.ForeignKey('Alcohol_Degree', on_delete=models.CASCADE)
+    alcohol_degree = models.ForeignKey('AlcoholDegree', on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'products'
 
-class product_Image(models.Model):
+class ProductImage(models.Model):
     image_url = models.URLField(max_length=2000)
     product   = models.ForeignKey(Product, on_delete=models.CASCADE)
 
@@ -24,7 +24,7 @@ class Category(models.Model):
     class Meta:
         db_table = 'categories'
 
-class category_Image(models.Model):
+class CategoryImage(models.Model):
     image_url = models.URLField(max_length=2000)
     category  = models.ForeignKey(Category, on_delete=models.CASCADE)
 
@@ -40,31 +40,31 @@ class Comment(models.Model):
     class Meta:
         db_table = 'comments'
 
-class Alcohol_Degree(models.Model):
-    percent = models.IntegerField(max_length=500)
+class AlcoholDegree(models.Model):
+    percent = models.IntegerField()
 
     class Meta:
         db_table = 'alcohol_degrees'
 
-class Finger_food(models.Model):
+class FingerFood(models.Model):
     name     = models.CharField(max_length=100)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'finger_foods'
 
-class finger_food_Image(models.Model):
+class FingerFoodImage(models.Model):
     image_url   = models.URLField(max_length=2000)
-    finger_food = models.ForeignKey(Finger_food, on_delete=models.CASCADE)
+    finger_food = models.ForeignKey(FingerFood, on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'finger_food_images'
 
 class Cart(models.Model):
-    user       = models.ForeignKey(User, on_delete=models.CASCADE)
-    product    = models.ForeignKey(Product, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-      
+    user      = models.ForeignKey(User, on_delete=models.CASCADE)
+    product   = models.ForeignKey(Product, on_delete=models.CASCADE, blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated   = models.DateTimeField(auto_now= True)
+
     class Meta:
         db_table = 'carts'
--- users/models.py
+++ users/models.py
@@ -5,7 +5,7 @@ class User(models.Model):
     email      = models.CharField(max_length=100, unique=True)
     password   = models.CharField(max_length=200)
     name       = models.CharField(max_length=30)
-    nick_name  = models.CharField(max_length=45)
+    nick_name  = models.CharField(max_length=45, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
